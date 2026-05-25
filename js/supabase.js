2d7c35146af887a48f6a8e476a38692fa92908b4
Carola van Dijk <voedselbankvoordieren010@gmail.com>
2026-05-25 15:15:12 +0200

    npm run build

--- js/supabase.js
+++ js/supabase.js
@@ -1,3 +1,13 @@
+import {
+  createClient
+} from "@supabase/supabase-js";
+
+const SUPABASE_URL =
+  "https://qybobqolknvovigickpy.supabase.co";
+
+const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5Ym9icW9sa252b3ZpZ2lja3B5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5NjQ5NDEsImV4cCI6MjA5MDU0MDk0MX0.UvZjGttYTUAZnr49m3AUeBoM1nfUHWykEhSnsh8iVa8";
+
 export const supabase =
   createClient(
 
