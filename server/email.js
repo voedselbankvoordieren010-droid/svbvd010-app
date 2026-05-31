8fb80475b46b884a2de53df07e9e3c4b1b40ae9c
Carola van Dijk <voedselbankvoordieren010@gmail.com>
2026-05-31 03:32:13 +0200

    npm run build

--- server/email.js
+++ server/email.js
@@ -1,3 +1,5 @@
+await transporter.verify();
+console.log("SMTP verified");
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
@@ -5,15 +7,15 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
+  port: 587,
   secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
   },
-  tls: {
-    ciphers: "TLSv1.2"
-  }
+  logger: true,
+  debug: true
 });
 
 export async function sendEmail({ to, subject, text, html }) {
