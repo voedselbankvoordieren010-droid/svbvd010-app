80d066e7b7571f815a52832bd531fcefbd870d15
Carola van Dijk <voedselbankvoordieren010@gmail.com>
2026-06-03 20:44:21 +0200

    npm run build

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,30 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './',
+
   build: {
+    sourcemap: false,
+
     chunkSizeWarningLimit: 1000,
+
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
+          if (!id.includes('node_modules')) return;
+
+          if (id.includes('@supabase')) {
+            return 'supabase';
           }
+
+          if (id.includes('pdf')) {
+    return 'pdf';
+  }
+  
+          if (id.includes('chart')) {
+            return 'charts';
+          }
+
+          return 'vendor';
         }
       }
     }
